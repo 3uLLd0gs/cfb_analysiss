e74d801673c788fca64a23c2583b272371083ebc
Minsub Song <mminsub90@gmail.com>
2026-08-31 11:32:40 -0500

    Add YouTube Video

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Data With Minsub',
+  author: 'Minsub Song',
+  headerTitle: 'Data With Minsub',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
